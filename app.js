e9aadb8fdb0ae795d040344adad1b7bbc91f7268
percworld <percworld@gmail.com>
2021-06-05 19:24:04 -0600

    Hash out extra code

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const cors = require('cors');
-const path = require('path');
+//const cors = require('cors');
+//const path = require('path');
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
 // app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
 
 // app.locals.title = "Setlift";
 
@@ -86,6 +86,6 @@ app.get('/', (req, res) => { // send a get request to root directory ('/' is thi
     })
 })
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`${app.locals.title} Server started on ${PORT}`))
